e44d5b7d4068e46bc4ff397615faa08147e21f1d
unknown <unidrelyn@gmail.com>
2021-09-10 16:41:00 +0200

    final

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import styled from "@emotion/styled";
 import imagen from "./cryptomonedas.png";
 import Formulario from "./components/Formulario";
 import Cotizacion from "./components/Cotizacion";
+import Spinner from "./components/Spinner";
 
 const Contenedor = styled.div`
 	max-width: 900px;
@@ -42,6 +43,7 @@ function App() {
 	const [moneda, setMoneda] = useState("");
 	const [criptomoneda, setCriptomoneda] = useState("");
 	const [resultado, setResultado] = useState({});
+	const [cargando, setCargando] = useState(false);
 
 	useEffect(() => {
 		const cotizarCriptomoneda = async () => {
@@ -50,10 +52,22 @@ function App() {
 			const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
 			const resultado = await axios.get(url);
-			setResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+
+			setCargando(true);
+
+			setTimeout(() => {
+				setCargando(false);
+				setResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+			}, 3000);
 		};
 		cotizarCriptomoneda();
 	}, [moneda, criptomoneda]);
+
+	const componente = cargando ? (
+		<Spinner />
+	) : (
+		<Cotizacion resultado={resultado} />
+	);
 
 	return (
 		<Contenedor>
@@ -63,7 +77,7 @@ function App() {
 			<div>
 				<Heading>Cotizador Cryptomonedas al instante</Heading>
 				<Formulario setCriptomoneda={setCriptomoneda} setMoneda={setMoneda} />
-				<Cotizacion resultado={resultado} />
+				{componente}
 			</div>
 		</Contenedor>
 	);
--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,13 +1,44 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Resultado = styled.div`
+	color: #fff;
+	font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+	font-size: 18px;
+	span {
+		font-weight: bold;
+	}
+`;
+const Precio = styled.p`
+	font-size: 30px;
+	span {
+		font-weight: bold;
+	}
+`;
 
 const Cotizacion = ({ resultado }) => {
 	if (Object.keys(resultado).length === 0) return null;
 	return (
-		<div>
-			<p>
+		<Resultado>
+			<Precio>
 				El precio es: <span>{resultado.PRICE}</span>
-			</p>
-		</div>
+			</Precio>
+			<Info>
+				El precio mas alto: <span>{resultado.HIGHDAY}</span>
+			</Info>
+			<Info>
+				El precio mas bajo: <span>{resultado.LOWDAY}</span>
+			</Info>
+			<Info>
+				La variacion 24Hr es: <span>{resultado.CHANGEPCT24HOUR}</span>
+			</Info>
+			<Info>
+				Ultima Actualizacion: <span>{resultado.LASTUPDATE}</span>
+			</Info>
+		</Resultado>
 	);
 };
 
